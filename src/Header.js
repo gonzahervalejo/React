5298c753b2a4475c15a8c4ed164168451e736172
Your Name <you@example.com>
2022-04-20 22:48:36 -0300

    primer commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./components/NavBar"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__titlo">E-Commerce</h1>
+      <Nav/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+import App from "../App"
+
+const NavBar = () => {
+    return (
+        <App>
+        <nav className="nav">
+          <a href="../index.html" className="nav__link">link 1</a>
+          <a href="../index.html" className="nav__link">link 2</a>
+          <a href="../index.html" className="nav__link">link 3</a>
+        </nav>
+        </App>
+      )
+}
+export default NavBar
